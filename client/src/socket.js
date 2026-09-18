21569b998c6b7bf4eff942b025ac1f5fb8b0f285
Dannywest223 <okorodannels95@gmail.com>
2026-09-18 17:18:32 +0200

    Point socket to Render backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,9 +1,10 @@
 import { io } from 'socket.io-client'
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000'
+const SERVER_URL = 'https://malaika-game.onrender.com'
 
 export const socket = io(SERVER_URL, {
   autoConnect: true,
+  transports: ['websocket', 'polling'],
 })
 
 socket.on('connect', () => console.log('✅ Socket connected:', socket.id))
